766e4f4e1df6110f4dee3fd6e5912825c2b2be01
mrtc0 <mrtc0.py@gmail.com>
2018-01-06 11:53:55 +0900

    fix path

--- sw/install-event/js/registerServiceworker.js
+++ sw/install-event/js/registerServiceworker.js
@@ -1,3 +1,3 @@
-navigator.serviceWorker.register('/sw.js')
+navigator.serviceWorker.register('/pwa/sw/install-event/sw.js')
     .then(reg => console.log('SW registered!', reg))
     .catch(err => console.log('Failed to regist', err));
--- sw/install-event/sw.js
+++ sw/install-event/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', event => {
   console.log('install event');
 
   event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/images/blue.jpg'))
+    caches.open('static-v1').then(cache => cache.add('/pwa/sw/install-event/images/blue.jpg'))
   );
 });
 
@@ -13,7 +13,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  if (url.origin == location.origin && url.pathname == '/images/red.jpg') {
-    event.respondWith(caches.match('/images/blue.jpg'));
+  if (url.origin == location.origin && url.pathname == '/pwa/sw/install-event/images/red.jpg') {
+    event.respondWith(caches.match('/pwa/sw/install-event/images/blue.jpg'));
   }
 });
